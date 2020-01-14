e5fc09dbb424c133e1cc938c06e195d7da16d4a7
sqlstad <sanderstad@gmail.com>
2020-01-14 13:18:32 +0100

    Initial commit

---
+++ Test1-Tests/Security/TestTest1.sql
@@ -0,0 +1,7 @@
+﻿CREATE SCHEMA [TestTest1]
+    AUTHORIZATION [dbo];
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'tSQLt.TestClass', @value = 1, @level0type = N'SCHEMA', @level0name = N'TestTest1';
+
