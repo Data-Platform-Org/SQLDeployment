6c6098e9966e49e2f9258aa785341ce834692738
Kevin Chant <kevin.chant@sogeti.com>
2020-09-25 17:59:18 +0200

    Changed comment

--- UnitTesting-Data/Stored Procedures/Table1_Create.sql
+++ UnitTesting-Data/Stored Procedures/Table1_Create.sql
@@ -2,6 +2,6 @@
 @param1 VARCHAR(100),@param2 VARCHAR(100),@param3 VARCHAR(100),@param4 VARCHAR(100),@param5 VARCHAR(100),@param6 VARCHAR(100),@param7 VARCHAR(100),@param8 VARCHAR(100),@param9 VARCHAR(100),@param10 VARCHAR(100),@param11 VARCHAR(100),@param12 VARCHAR(100)
 AS
 BEGIN
-    --Ta daa works with GitHub Actions for North East Azure UG now
+    --Ta daa works with GitHub Actions for SQL Saturday Denmark now
     SELECT 'Table1_Create'
 END
