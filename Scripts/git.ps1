e8b1a89a0f5c91ecae897f0d39b1a64364e47d49
Kevin Chant <kchant1975@hotmail.com>
2020-11-21 14:22:54 +0100

    Added new github repo

--- Scripts/git.ps1
+++ Scripts/git.ps1
@@ -1,3 +1,5 @@
 cd 'C:\Agents\Azure DevOps Duet\_work\1\s'
+
+git remote add github https://github.com/kevchant/dataMinds.git
 #
 #git push https://GIT_PAT@github.com/kevchant/SQLSaturdayOslo.git HEAD:main
