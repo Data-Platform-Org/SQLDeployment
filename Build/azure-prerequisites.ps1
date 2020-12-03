63c34b71449e0b3f8c7ce5f778a4a88bb075c84f
Kevin Chant <kchant1975@hotmail.com>
2020-12-03 22:04:55 +0000

    Put login into azure-prerequisites.pd1

--- Build/azure-prerequisites.ps1
+++ Build/azure-prerequisites.ps1
@@ -2,6 +2,6 @@
 
 foreach ($module in $modules) {
     Write-Host "Installing $module" -ForegroundColor Cyan
-    Install-Module $module -Force -Scope CurrentUser -SkipPublisherCheck
+    if (-not (Get-Module -ListAvailable | Where-Object {$_.Name -eq $module})) {Install-Module $module -Force -SkipPublisherCheck}
     Import-Module $module -Force -PassThru
 }
