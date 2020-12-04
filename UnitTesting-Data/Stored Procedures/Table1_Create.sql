289b1b9299e9097736cc70644f439eb8d3f9cb15
Kevin Chant <kchant1975@hotmail.com>
2020-12-04 12:06:10 +0100

    speed test

--- UnitTesting-Data/Stored Procedures/Table1_Create.sql
+++ UnitTesting-Data/Stored Procedures/Table1_Create.sql
@@ -2,6 +2,6 @@
 @param1 VARCHAR(100),@param2 VARCHAR(100),@param3 VARCHAR(100),@param4 VARCHAR(100),@param5 VARCHAR(100),@param6 VARCHAR(100),@param7 VARCHAR(100),@param8 VARCHAR(100),@param9 VARCHAR(100),@param10 VARCHAR(100),@param11 VARCHAR(100),@param12 VARCHAR(100)
 AS
 BEGIN
-    --Speed test 12
+    --Speed test 14
     SELECT 'Table1_Create'
 END
